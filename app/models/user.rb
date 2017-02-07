10beba58ee879c684e6a20699e1f9716f40040d9
Sid Ahmed Merzouk <email": null>
2017-02-07 12:48:10 -0500

    add associations & fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :username, :email, presence: true
   validates :password, length: { minimum: 6 }
-
+  has_many :restaurants
+  
   has_secure_password
 end
