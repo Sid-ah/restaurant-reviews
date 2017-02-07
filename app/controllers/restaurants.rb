ce0ad505b195c15e4a90f0c62eae1dc4742db052
Sid Ahmed Merzouk <email": null>
2017-02-07 13:16:21 -0500

    create a new restaurant

---
+++ app/controllers/restaurants.rb
@@ -0,0 +1,20 @@
+get '/restaurants/new' do
+  require_user
+  erb :'restaurants/new'
+end
+
+post '/restaurants' do
+  @restaurant = current_user.restaurants.new(params[:restaurant])
+  if @restaurant.save
+    redirect "/restaurants/#{@restaurant.id}"
+  else
+    @errors = @restaurant.errors.full_messages
+    erb :'restaurants/new'
+  end
+end
+
+get '/restaurants/:id' do
+  require_user
+  @restaurant = Restaurant.find_by(id: params[:id])
+  erb :'restaurants/show'
+end
