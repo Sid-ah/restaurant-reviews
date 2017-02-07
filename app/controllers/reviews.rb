fe1cf4ed6be784f546f66401212e6d7c763689f8
Sid Ahmed Merzouk <email": null>
2017-02-07 17:09:53 -0500

    done

--- app/controllers/reviews.rb
+++ app/controllers/reviews.rb
@@ -8,3 +8,16 @@ post '/reviews' do
     erb :'restaurants/show'
   end
 end
+
+post '/reviews/login' do
+  @restaurant = Restaurant.find(params[:restaurant_id])
+  @user = User.find_by(email: params[:user][:email])
+  @reviews = @restaurant.reviews
+  if @user &&  @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/restaurants/#{@restaurant.id}"
+  else
+    @errors= ['Invalid username/password']
+    erb :'restaurants/show'
+  end
+end
